6ee8cd40c2f77adbdc0cb8c6c3136468ac92a4a3
Dean Collier <daccollier@gmail.com>
2021-10-12 11:59:59 -0400

    finished up products

--- GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
 
 namespace GeneralStore.MVC.Controllers
 {
@@ -17,7 +18,9 @@ namespace GeneralStore.MVC.Controllers
         // GET: Product
         public async Task<ActionResult> Index()
         {
-            return View(await _db.Products.ToArrayAsync());
+            Product[] productArray = await _db.Products.ToArrayAsync();
+            Product[] orderedArray = productArray.OrderBy(prod => prod.Name).ToArray();
+            return View(orderedArray);
         }
 
         // GET: Product 
@@ -42,30 +45,18 @@ namespace GeneralStore.MVC.Controllers
 
         }
 
-        // GET: Delete
-        // Product/Delete/{id}
-        public async Task<ActionResult> Delete(int? id)
+        // GET: Details
+        //Product/Details/{id}
+        public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
-            {
-                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
-            }
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
             Product product = await _db.Products.FindAsync(id);
             if (product == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(product);
-        }
-        // Post: Delete
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Delete(int id)
-        {
-            Product product = await _db.Products.FindAsync(id);
-            _db.Products.Remove(product);
-            await _db.SaveChangesAsync();
-            return RedirectToAction("Index");
         }
 
         // GET: Edit
@@ -73,14 +64,12 @@ namespace GeneralStore.MVC.Controllers
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
-            {
-                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
-            }
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
             Product product = await _db.Products.FindAsync(id);
             if (product == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(product);
         }
         // POST: Edit
@@ -95,6 +84,30 @@ namespace GeneralStore.MVC.Controllers
                 return RedirectToAction("Index");
             }
             return View(product);
+        }
+
+        // GET: Delete
+        // Product/Delete/{id}
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Product product = await _db.Products.FindAsync(id);
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
+        }
+        // Post: Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Product product = await _db.Products.FindAsync(id);
+            _db.Products.Remove(product);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
